fa9d3360c5f0dd90145a49e3e8f5ce761a4e3d95
rosendo <rosendc30@gmail.com>
2023-05-13 10:11:16 -0300

    forst commit

---
+++ main.js
@@ -0,0 +1,74 @@
+alert("hello word");
+
+let x = 2;
+let y = 2;
+
+let total = x + y;
+
+alert("O total da soma entre " + x + " e " + y + " é: " + total);
+
+if (total % 2 == 0) {
+  alert(total + " é par");
+} else {
+  alert(total + " é impar");
+}
+
+let z = 0;
+if (typeof z == "number") {
+  alert(z + " é um número");
+} else {
+  alert(z + " não é um número");
+}
+
+let a = "rocketseat";
+if (typeof a == "string") {
+  alert(a + " é um string");
+} else {
+  alert(a + " não é um string");
+}
+
+let b = true;
+if (typeof b == "boolean") {
+  alert(b + " é um booleano");
+} else {
+  alert(b + " não é um booleano");
+}
+
+let x2 = 20;
+let y2 = 17;
+
+let totalSubtracao = x2 - y2;
+
+alert(
+  "O total da Subtracao entre " + x2 + " e " + y2 + " é: " + totalSubtracao
+);
+
+let x3 = 20;
+let y3 = 17;
+
+let totalMultiplicacao = x2 * y2;
+
+alert(
+  "O total da Multiplicacao entre " +
+    x3 +
+    " e " +
+    y3 +
+    " é: " +
+    totalMultiplicacao
+);
+
+let x4 = 20;
+let y4 = 5;
+
+let totalDisivao = x4 / y4;
+
+alert("O total da Disivao entre " + x4 + " e " + y4 + " é: " + totalDisivao);
+
+
+
+let calc = 100;
+if (calc % 2 == 0) {
+  alert(total + " é um número par");
+} else {
+  alert(total + " não é um número par");
+}
